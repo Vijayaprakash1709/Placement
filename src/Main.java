db2585d2476f0c4ab76e6d5d0cd58a92d8d5e3d4
vijayaprakash1709 <vijayaprakash885@gmail.com>
2024-04-29 21:05:26 +0530

    Main commit

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,8 @@
 import java.util.*;
 public class Main {
     public static void main(String[] args ) {
-        Scanner sc = new Scanner(System.in);
-        int n=sc.nextInt();
-        String s="";
-        while(n>0) {
-            int rem = n % 2;
-            s = rem + s;
-            n = n / 2;
-        }
-        System.out.println(s);
-        String rev="";
-        for(int i=s.length()-1;i>=0;i--){
-            rev=rev+s.charAt(i);
-        }
-        if(s.equals(rev)){
-            System.out.println("Palindrome");
-        }
-        else{
-            System.out.println("It is not a Palindrome");
-        }
-
+        int a=10,b=20;
+        System.out.println(++a + ++b + b);
+        //10 a++ = 10
     }
 }
