09ae4680264cd3f3d3b8f2286fc490bcad21b51b
vijayaprakash1709 <vijayaprakash885@gmail.com>
2024-04-29 20:01:38 +0530

    1 st question

---
+++ src/SumOfNnumbers.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class SumOfNnumbers {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        //best approach to solve sum of n natural numbers
+        System.out.println(n*(n+1)/2);
+    }
+}
