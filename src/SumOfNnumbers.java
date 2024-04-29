68115db6980acc44e0e1f945f44df744ef032d5d
vijayaprakash1709 <vijayaprakash885@gmail.com>
2024-04-29 20:54:39 +0530

    1st  question

--- src/SumOfNnumbers.java
+++ src/SumOfNnumbers.java
@@ -1,10 +1,20 @@
-import java.util.*;
-public class SumOfNnumbers {
+import java.util.Scanner;
+public class SumOfNnumbers{
     public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
+        //sc = reference
+        //Student name,id
+        //name =new Student()
+        // Student note=new Student()
+        Scanner sc = new Scanner(System.in);
+        // class ref = new constructor(parameter);
         int n=sc.nextInt();
-        //best approach to solve sum of n natural numbers
+        int sum=0;
+//        for(int i=1;i<=n;i++){
+//            //O(n)
+//            sum=sum+i;
+//        }
+        sum=n*(n+1)/2;
+        System.out.println(sum);
 
-        System.out.print(n*(n+1)/2);
     }
 }
